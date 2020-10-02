713c6af155611051fc9b5b33112363f00550c357
George Spasov <spasov05203@gmail.com>
2020-10-02 10:25:20 +0300

    Created SDK For liqudity provision and similar functions

---
+++ sdk/src/sdk.js
@@ -0,0 +1,152 @@
+const ethers = require('ethers');
+const { Token, WETH, Fetcher, Route, Trade, TokenAmount, TradeType, Percent } = require('@uniswap/sdk')
+const uniswapRouterABI = require('./UniswapRouterABI.json');
+const uniswapLiquidityPoolTokenABI = require('./UniswapLiquidityPoolTokenABI.json')
+
+const uniswapV2RouterAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+
+class ALBTStakerSDK {
+
+	constructor(provider, contractsConfig, debug) {
+    	this.provider = provider;
+    	this.contractsConfig = contractsConfig;
+		this.debug = debug;
+  	}
+
+	async getUniswapPairOtherTokenAmount(tokenAName, tokenBName, tokenAAmount) {
+		const network = await this.provider.getNetwork() 
+		if(this.debug) {
+			console.log(network);
+		}
+
+		const tokenA = await this._getUniswapTokenByName(tokenAName);
+		const tokenB = await this._getUniswapTokenByName(tokenBName);
+
+		const pair = await Fetcher.fetchPairData(tokenB, tokenA)
+		const route = new Route([pair], tokenA)
+
+		const trade = new Trade(route, new TokenAmount(tokenA, tokenAAmount), TradeType.EXACT_INPUT)
+
+		if(this.debug) {
+			console.log("Execution Price:", trade.executionPrice.toSignificant(6))
+		}
+
+		return { tokenAmount:trade.outputAmount.toFixed().toString(), tokenInfo: tokenB }; // TODO Non HR output amount ?
+	}
+
+	async getUniswapRouterTokenApproval(wallet, tokenName) {
+		if(this.isETH(tokenName)) {
+			return ethers.constants.MaxUint256
+		}
+		const token = await this._getUniswapTokenByName(tokenName);
+		const tokenContract = new ethers.Contract(token.address, uniswapLiquidityPoolTokenABI, wallet);
+		return tokenContract.allowance(wallet.address, uniswapV2RouterAddress)
+	}
+
+	async approveUniswapRouterForToken(wallet, tokenName) {
+		if(this.isETH(tokenName)) {
+			throw new Error("No need to approve for ETH")
+		}
+		const token = await this._getUniswapTokenByName(tokenName);
+		const tokenContract = new ethers.Contract(token.address, uniswapLiquidityPoolTokenABI, wallet);
+		return tokenContract.approve(uniswapV2RouterAddress, ethers.constants.MaxUint256)
+	}
+
+	async addUniswapLiquidity(wallet, tokenAName, tokenBName, tokenAAmount, tokenBAmount) {
+
+		const routerContract = new ethers.Contract(uniswapV2RouterAddress, uniswapRouterABI, wallet)
+
+		// Default Slippage is 0.5%
+		const tokenAAmountBN = ethers.utils.bigNumberify(tokenAAmount);
+		const tokenAAmountBNSlip = tokenAAmountBN.mul(50).div(10000)
+		const tokenAAmountMinBN = tokenAAmountBN.sub(tokenAAmountBNSlip)
+
+		const tokenBAmountBN = ethers.utils.bigNumberify(tokenBAmount);
+		const tokenBAmountBNSlip = tokenBAmountBN.mul(50).div(10000)
+		const tokenBAmountMinBN = tokenBAmountBN.sub(tokenBAmountBNSlip)
+
+
+		console.log("Token A", tokenAAmountBN.toString(), tokenAAmountMinBN.toString())
+		console.log("Token B", tokenBAmountBN.toString(), tokenBAmountMinBN.toString())
+
+		const deadline = Math.floor(Date.now()/1000) + (60*60)
+
+		let transaction;
+
+		if(this.isETH(tokenAName)) {
+			const tokenB = await this._getUniswapTokenByName(tokenBName);
+			transaction = await routerContract.addLiquidityETH(tokenB.address, tokenBAmountBN, tokenBAmountMinBN, tokenAAmountMinBN, wallet.address, deadline, {value: tokenAAmountBN})
+			
+		} else {
+			const tokenA = await this._getUniswapTokenByName(tokenAName);
+			const tokenB = await this._getUniswapTokenByName(tokenBName);
+			transaction = await routerContract.addLiquidity(tokenA.address, tokenB.address, tokenAAmountBN, tokenBAmountBN, tokenAAmountMinBN, tokenBAmountMinBN, wallet.address, deadline);
+		}
+
+		return transaction;
+	}
+
+	async getBalance(wallet, tokenName) {
+		if(this.isETH(tokenName)) {
+			return wallet.getBalance()
+		}
+
+		const token = await this._getUniswapTokenByName(tokenName);
+		const tokenContract = new ethers.Contract(token.address, uniswapLiquidityPoolTokenABI, wallet)
+
+		const walletAddress = await wallet.getAddress();
+
+		return tokenContract.balanceOf(walletAddress)
+	}
+
+	async getUniswapPoolTokenBalance(wallet, tokenAName, tokenBName) {
+		const poolTokenAddress = this._getUniswapPairPoolToken(tokenAName, tokenBName);
+		const tokenContract = new ethers.Contract(poolTokenAddress, uniswapLiquidityPoolTokenABI, wallet)
+
+		const walletAddress = await wallet.getAddress();
+
+		return tokenContract.balanceOf(walletAddress)
+	}
+
+	// --- Internal functions ---
+
+	isETH(tokenName) {
+		return tokenName == 'ETH'
+	}
+
+	async _getUniswapTokenByName(tokenName) {
+		const network = await this.provider.getNetwork() 
+		const chainId = network.chainId;
+		if(this.isETH(tokenName)) {
+			return WETH[chainId]
+		}
+
+		const tokenAddress = this.contractsConfig.tokenContracts[tokenName];
+		if(typeof tokenAddress == 'undefined') {
+			throw new Error('No such token found in the configuration')
+		}
+
+		return Fetcher.fetchTokenData(chainId, tokenAddress)
+	}
+
+
+	_getUniswapPairPoolToken(tokenAName, tokenBName) {
+		let poolTokenKey = `${tokenAName}-${tokenBName}`;
+		let pairToken = this.contractsConfig.uniswap.poolTokens[poolTokenKey];
+		if(typeof pairToken != 'undefined') {
+			return pairToken
+		}
+
+		poolTokenKey = `${tokenBName}-${tokenAName}`;
+		pairToken = this.contractsConfig.uniswap.poolTokens[poolTokenKey];
+		if(typeof pairToken != 'undefined') {
+			return pairToken
+		}
+
+		throw new Error('No such pair found')
+	}
+	
+
+}
+
+module.exports = ALBTStakerSDK
---
+++ sdk/test/test.js
@@ -0,0 +1,119 @@
+const ethers = require('ethers');
+const ALBTStakerSDK = require('./../src/sdk');
+
+const contractsConfig = {
+	uniswap: {
+		pairs: [
+			"ETH-DAI",
+			"WETH-DAI",
+			"ETH-USDC",
+			"WETH-USDC",
+			"ETH-UNI",
+			"WETH-UNI",
+			"UNI-DAI",
+			"UNI-USDC"
+		],
+		poolTokens: {
+			"ETH-DAI": "0xa478c2975ab1ea89e8196811f51a7b7ade33eb11",
+			"WETH-DAI": "0xa478c2975ab1ea89e8196811f51a7b7ade33eb11",
+			"ETH-USDC": "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc",
+			"WETH-USDC": "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc",
+			"ETH-UNI": "0xd3d2e2692501a5c9ca623199d38826e513033a17",
+			"WETH-UNI": "0xd3d2e2692501a5c9ca623199d38826e513033a17",
+			"UNI-DAI": "0xf00e80f0de9aea0b33aa229a4014572777e422ee",
+			"UNI-USDC": "0xebfb684dd2b01e698ca6c14f10e4f289934a54d6"
+		}
+	},
+	tokenContracts: {
+		"WETH": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+		"DAI": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+		"UNI": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+		"USDC": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+	},
+}
+
+const run = async () => {
+
+	// Setting up the test
+	// This is the pair tokens that we are going to pool
+	const tokenA = 'UNI'
+	const tokenB = 'DAI'
+
+	const tokenAAmount = ethers.utils.parseEther('1').toString(10) // We only know one of the amounts we want to pool and the other one will be calculated
+
+
+	// Set up the provider. With Metamask it would be Web3Provider
+	const mainnetProvider = new ethers.providers.InfuraProvider(1, '40c2813049e44ec79cb4d7e0d18de173')
+	const localProvider = new ethers.providers.JsonRpcProvider('http://localhost:8545') // Using local provider in order not to spend money	
+
+	// Set up the sdk
+	const sdk = new ALBTStakerSDK(localProvider, contractsConfig, true)
+
+	// Set up the user wallet
+	let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, localProvider)
+
+	const tokenABalance = await sdk.getBalance(wallet, tokenB);
+	if(tokenABalance.lt(tokenAAmount)) {
+		throw new Error(`Not enough ${tokenA} for this liquidity provision`)
+	}
+
+	// Get the amount of token B based on how much you input for token A
+	const tokenBAmountInfo = await sdk.getUniswapPairOtherTokenAmount(tokenA, tokenB, tokenAAmount)
+	console.log("Counter token info", tokenBAmountInfo)
+
+	// Parse it into "wei"
+	const tokenBAmount = ethers.utils.parseUnits(tokenBAmountInfo.tokenAmount, tokenBAmountInfo.tokenInfo.decimals).toString()
+
+	// Check if you have enough balance
+	const tokenBBalance = await sdk.getBalance(wallet, tokenB);
+	if(tokenBBalance.lt(tokenBAmount)) {
+		throw new Error(`Not enough ${tokenB} for this liquidity provision`)
+	}
+
+	// Get how many Liquidity pool tokens do you have now
+	const LPTokensBefore = await sdk.getUniswapPoolTokenBalance(wallet, tokenA, tokenB);
+	console.log("Liqudity Pool Tokens before addition", ethers.utils.formatEther(LPTokensBefore.toString(10)))
+
+	// Check if you have enough approval for tokenA
+	const tokenAApproval = await sdk.getUniswapRouterTokenApproval(wallet, tokenA);
+
+	console.log(`${tokenA} Approval`, tokenAApproval.toString(10))
+
+	// Approving if no enough approval for the liquidity provision
+	if(tokenAApproval.lt(tokenAAmount)) {
+		console.log(`Not enough approval for ${tokenA}`);
+		const approveTransaction = await sdk.approveUniswapRouterForToken(wallet, tokenA);
+		console.log("Approval Transaction", approveTransaction.hash)
+		const approveReceipt = await approveTransaction.wait();
+		console.log("Approval transaction status", approveReceipt.status); // should be 1
+	}
+
+	// Check if you have enough approval for tokenB
+	const tokenBApproval = await sdk.getUniswapRouterTokenApproval(wallet, tokenB);
+
+	console.log(`${tokenB} Approval`, tokenBApproval.toString(10))
+
+	// Approving if no enough approval for the liquidity provision
+	if(tokenBApproval.lt(tokenBAmount)) {
+		console.log(`Not enough approval for ${tokenB}`);
+		const approveTransaction = await sdk.approveUniswapRouterForToken(wallet, tokenB);
+		console.log("Approval Transaction", approveTransaction.hash)
+		const approveReceipt = await approveTransaction.wait();
+		console.log("Approval transaction status", approveReceipt.status); // should be 1
+	}
+
+	// Providing Uniswap Liquidity
+	const transaction = await sdk.addUniswapLiquidity(wallet, tokenA, tokenB, tokenAAmount, tokenBAmount)
+
+	console.log("Add liqudity transaction", transaction.hash)
+
+	const receipt = await transaction.wait();
+	console.log("Add liquidity transaction status", receipt.status); // should be 1
+
+	// Checking the new LP Tokens balance
+	const LPTokensAfter = await sdk.getUniswapPoolTokenBalance(wallet, tokenA, tokenB);
+	console.log("Liqudity Pool Tokens before addition", ethers.utils.formatEther(LPTokensAfter.toString(10)))
+
+}
+
+run()
