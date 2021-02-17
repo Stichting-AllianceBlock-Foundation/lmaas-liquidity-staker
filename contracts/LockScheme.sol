af1c3a466fbf3ec0ec6a5a5e1b127b630b47a694
maptuhec <martin@limechain.tech>
2021-02-17 17:28:59 +0200

    add coverage to ignore, initial skeleton

--- contracts/LockScheme.sol
+++ contracts/LockScheme.sol
@@ -1,5 +1,10 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 
 contract LockScheme {
-	
+    uint256 public lockPeriod; // The period of lock for this contract
+    uint256 public rampUpPeriod; // The period since the beginning of the lock that additions can be considered the same position.Might be 0 for the 0% lock periods
+
+    uint256 public bonusPercent;
+    address public lmcContract; // The address of the lmc contract
 }
