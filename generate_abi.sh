a6c7acaa75f6ac10ed3896a3383b217ac578cd45
Mihai Oprea <25897491+mehiX@users.noreply.github.com>
2022-01-30 16:44:26 +0100

    simplify the generation, use (almost) the same script as for other contract projects

--- generate_abi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/bin/solc openzeppelin-solidity/=./node_modules/openzeppelin-solidity/ --abi ./contracts/LiquidityMiningCampaign.sol --allow-paths '' -o ./build
-
-#for f in `ls ./contracts/*.sol`; do 
-#    /usr/bin/solc openzeppelin-solidity/=./node_modules/openzeppelin-solidity/ --abi $f --allow-paths '' -o ./build
-#done
-
